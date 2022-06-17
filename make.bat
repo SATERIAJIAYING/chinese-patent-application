ac48673b8023a400604df9ef278895f5cc01db79
SATERIAJIAYING <82074412+SATERIAJIAYING@users.noreply.github.com>
2022-06-17 23:11:01 +0800

    Add files via upload

---
+++ make.bat
@@ -0,0 +1,24 @@
+del 最终报告.pdf
+del 专利申请书.pdf
+del 课程考试成绩单.pdf
+xelatex 专利申请书 &
+xelatex 专利申请书
+xelatex 课程考试成绩单 &
+xelatex 课程考试成绩单
+xelatex 最终报告 &
+del *.aux /s
+del *.bak /s
+del *.log /s
+del *.bbl /s
+del *.dvi /s
+del *.blg /s
+del *.thm /s
+del *.toc /s
+del *.out /s
+del *.lof /s
+del *.lol /s
+del *.lot /s
+del *.nav /s
+del *.snm /s
+del *.fdb_latexmk /s
+del *.synctex.gz /s
